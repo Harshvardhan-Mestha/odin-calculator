348c837f5d7ef8c473f016735ff9d897563d8b0a
Harshvardhan Mestha <harshvardhanmestha@yahoo.com>
2023-01-12 16:03:17 +0530

    This commit adds support for floating point numbers.

--- script.js
+++ script.js
@@ -7,27 +7,29 @@ let y = 0;
 let operator = ""
 
 let opr_btns = document.querySelectorAll('button.opr')
-opr_btns.forEach(opr_btn => opr_btn.addEventListener('click',operand))
+opr_btns.forEach(opr_btn => opr_btn.addEventListener('mousedown',operand))
 
 let num_btns = document.querySelectorAll('button.num')
-num_btns.forEach(num_btn => num_btn.addEventListener('click',display_num))
+num_btns.forEach(num_btn => num_btn.addEventListener('mousedown',display_num))
 
 let clr_btn = document.getElementById('clr')
-clr_btn.addEventListener('click',clear)
+clr_btn.addEventListener('mousedown',clear)
 
 let bks_btn = document.getElementById('bks')
-bks_btn.addEventListener('click',backspace)
+bks_btn.addEventListener('mousedown',backspace)
 
 let sgn_btn = document.getElementById('sgn')
-sgn_btn.addEventListener('click',sign)
+sgn_btn.addEventListener('mousedown',sign)
 
 let per_btn = document.getElementById('per')
-per_btn.addEventListener('click',percent)
+per_btn.addEventListener('mousedown',percent)
 
 let eql_btn = document.getElementById('eql')
-eql_btn.addEventListener('click',operate)
+eql_btn.addEventListener('mousedown',operate)
 
-
+let dec_btn = document.getElementById('dec')
+dec_btn.removeEventListener('mousedown',display_num)
+dec_btn.addEventListener('mousedown',decimal)
 
 
 clear()
@@ -37,8 +39,11 @@ function display_num(e){
     if (disp.textContent == "0" || opr_flg == true){
         disp.textContent = ""
         opr_flg = false
+        pt_flg = false
     }
+    
     disp.textContent = disp.textContent + e.target.id
+    trim_output()
 }
 
 function operand(e){
@@ -60,7 +65,6 @@ function clear(){
 function backspace(){
     disp.textContent = disp.textContent.slice(0,-1)
     if (disp.textContent == "" || disp.textContent == "-"){clear()}
-    
 }
 
 function sign(){
@@ -70,10 +74,20 @@ function sign(){
 
 function percent(){
     let x = Number(disp.textContent)
-    disp.textContent = x/100
+    disp.textContent = parseFloat(x/100)
 }
 
+function decimal(){
+    if(pt_flg == false){
+        disp.textContent = disp.textContent + '.'
+        pt_flg = true;
+    }
+}
+
+
+
 function operate(){
+
     
     y  = Number(disp.textContent)
     if(operator == "add"){
@@ -90,10 +104,23 @@ function operate(){
     }
 
     opr_btns.forEach(opr_btn => opr_btn.classList.remove('opr_hvr'))
+    //trim_output()
     disp.textContent = ans
+    if (String(ans).length>=14){
+        ans = ans.toExponential(7)
+        disp.textContent = parseFloat(ans)
+    }
+    console.log(parseFloat(ans))
     operator = "done"
     
 }
+
+function trim_output(){
+    if (String(disp.textContent).length >= 14){
+        disp.textContent = disp.textContent.slice(0,13)
+    }
+}
+
 
 
 
